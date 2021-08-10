ae8534ec7aa501dc6e3d2991e4d979aa20e9e55f
francis <lxq17next@gmail.com>
2021-08-09 20:15:43 -0700

    Basic Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,35 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { UserInfo } from './userinfo';
 import { UserPostService } from './user-post.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent  implements OnInit{
-  title = 'proj';
-   userInfo:UserInfo={
-     userName:"",
-     userPwd:""
-   };
+export class AppComponent implements OnInit {
+    title = "USER SYSTEM";
+    userInfo: UserInfo = {
+        userName: "",
+        userPwd: ""
+    };
 
-  constructor(private userPostService:UserPostService){}
-  ngOnInit():void{
-  }
+    Res = ""
 
-  onLogin(){
-  this.userPostService.logIn(this.userInfo).subscribe(
-    next=>{
-console.log('Login result:',next)
-  });
-  }
-  onRegister(){
-    this.userPostService.Register(this.userInfo).subscribe(
-      next=>{
-        console.log('Insert result:',next)
-      }
-    )
+    constructor(private userPostService: UserPostService) { }
+    ngOnInit(): void {
+    }
 
-  }
+    onLogin() {
+        this.userPostService.logIn(this.userInfo).subscribe(
+            next => {
+                console.log('Login result:', next)
+                this.Res = next.Login
+            });
+    }
+    onRegister() {
+        this.userPostService.Register(this.userInfo).subscribe(
+            next => {
+                console.log('Insert result:', next)
+                this.Res = next.Register
+            }
+        )
+
+    }
 
 }
