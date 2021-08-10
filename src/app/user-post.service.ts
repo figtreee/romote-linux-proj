44ed3e9aa8bfd6ac207e21ef3a80a917febfc693
figtreee <lxq17next@gmail.com>
2021-08-10 02:35:57 -0700

    Login method post

--- src/app/user-post.service.ts
+++ src/app/user-post.service.ts
@@ -4,18 +4,18 @@ import { UserInfo } from './userinfo';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserPostService {
 
-  constructor( private http: HttpClient,) { }
+    constructor(private http: HttpClient,) { }
 
-  
-  logIn(userInfo:UserInfo):Observable<any>{
-return this.http.get<any>(`/demo/api/user/login?name=${userInfo.userName}&pwd=${userInfo.userPwd}`).pipe()
-  }
 
-  Register(userInfo:UserInfo):Observable<any>{ 
-    return this.http.post<any>(`/demo/api/user/register`,userInfo).pipe()
- }
+    logIn(userInfo: UserInfo): Observable<any> {
+        return this.http.post<any>(`/demo/api/user/login`, userInfo).pipe()
+    }
+
+    Register(userInfo: UserInfo): Observable<any> {
+        return this.http.post<any>(`/demo/api/user/register`, userInfo).pipe()
+    }
 }
